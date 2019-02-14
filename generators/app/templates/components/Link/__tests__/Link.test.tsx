175bc5f55e545d4cc370759b9013aa6873761134
Maciej Stefaniak <maciej.stefaniak@insisto.pl>
2019-02-14 14:06:34 +0100

    Refactored tests structure Modified Link component tests

---
+++ generators/app/templates/components/Link/__tests__/Link.test.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { mount, shallow, render } from 'enzyme' // 'mount' for testing full lifecycle
+import { default as Link } from './../Link'
+const { Link: InternalLink } = require('./../../../server/routes')
+
+describe('Link component', () => {
+
+  it('Is internal link rendered properly', () => {
+    const wrapper = mount(<Link type='internal' url='email@domain.com' title='title' />)
+
+    expect(wrapper.find(InternalLink).length).toEqual(1)
+    expect(wrapper.find('a span').text()).toBe('title')
+  })
+
+  it('Is mailto link rendered properly', () => {
+    const wrapper = mount(<Link type='mailto' url='email@domain.com' title='title' />)
+
+    expect(wrapper.find('a').text()).toBe('title')
+    expect(wrapper.find('a').props().href).toBe('email@domain.com')
+  })
+
+  it('Is external link rendered properly', () => {
+    const wrapper = mount(<Link type='external' url='http://domain.com' title='External link' />)
+    
+    expect(wrapper.find('a').props().href).toBe('http://domain.com')
+    expect(wrapper.find('a').props().rel).toBe('noopener')
+    expect(wrapper.find('a').props().target).toBe('_blank')
+  })
+})
