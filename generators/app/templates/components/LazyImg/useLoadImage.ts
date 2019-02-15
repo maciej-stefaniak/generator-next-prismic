04aedb973215209609f255aea76a13212dda4621
Jon Ander Pérez <jonanderdev@gmail.com>
2019-02-15 09:56:18 +0100

    Add default return to useLoadImage

--- generators/app/templates/components/LazyImg/useLoadImage.ts
+++ generators/app/templates/components/LazyImg/useLoadImage.ts
@@ -35,6 +35,8 @@ export default (props: ILazyImgProps): ILazyImgState => {
             inlineSvgFallback: action.payload.inlineSvgFallback,
             status: 'LOADED'
           }
+        default:
+          return state
       }
     },
     {
@@ -47,36 +49,39 @@ export default (props: ILazyImgProps): ILazyImgState => {
   /*
    * Starts/triggers the loading of the image source
    */
-  useEffect(() => {
-    if (src) {
-      if (inlineSvg) {
-        loadInlineSvg(src, props, dispatch)
-      } else {
-        const imgElem = document.createElement('img')
-        imgElem.onload = () => dispatch({ type: 'LOADED' })
-        imgElem.onerror = e =>
-          dispatch({ type: 'FAILED', payload: { error: e } })
-        imgElem.src = src
+  useEffect(
+    () => {
+      if (src) {
+        if (inlineSvg) {
+          loadInlineSvg(src, props, dispatch)
+        } else {
+          const imgElem = document.createElement('img')
+          imgElem.onload = () => dispatch({ type: 'LOADED' })
+          imgElem.onerror = e =>
+            dispatch({ type: 'FAILED', payload: { error: e } })
+          imgElem.src = src
 
-        if (timeout) {
-          loadTimeout = setTimeout(
-            () => dispatch({ type: 'FAILED', payload: { error: 'timeout' } }),
-            timeout
-          )
-        }
+          if (timeout) {
+            loadTimeout = setTimeout(
+              () => dispatch({ type: 'FAILED', payload: { error: 'timeout' } }),
+              timeout
+            )
+          }
 
-        if (!isNode && isIE()) {
-          if (imgElem.complete) {
-            dispatch({ type: 'LOADED' })
+          if (!isNode && isIE()) {
+            if (imgElem.complete) {
+              dispatch({ type: 'LOADED' })
+            }
           }
         }
       }
-    }
 
-    return () => {
-      clearTimeout(loadTimeout)
-    }
-  }, [src])
+      return () => {
+        clearTimeout(loadTimeout)
+      }
+    },
+    [src]
+  )
 
   return state as ILazyImgState
 }
