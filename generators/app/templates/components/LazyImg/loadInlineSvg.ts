9bdfa7109117dd80b0e634ab03ca4c30e335c649
Jon Ander Perez <jonanderdev@gmail.com>
2019-09-19 09:47:12 +0200

    Make inline svgs to fetch local first

--- generators/app/templates/components/LazyImg/loadInlineSvg.ts
+++ generators/app/templates/components/LazyImg/loadInlineSvg.ts
@@ -1,19 +1,54 @@
+const POSSIBLE_LOCAL_SVGS = []
+
+const hasSVGLocalFallback = url => {
+  let newURL = url
+  for (let index = 0; index < POSSIBLE_LOCAL_SVGS.length; index++) {
+    const possibleHeroSvgLocal = POSSIBLE_LOCAL_SVGS[index]
+    if (url && url.includes(`${possibleHeroSvgLocal}.svg`)) {
+      newURL = `/static/images/inline-svg-fallbacks/${possibleHeroSvgLocal}.svg`
+    }
+  }
+
+  return newURL
+}
+
 /*
  * Try to loads the svg as inline, to allow animations, style changes and such
  */
-export default (src: string, { inlineSvgFallback = null }, dispatch) => {
+export default (
+  src: string,
+  { inlineSvgFallback = null, addAspectRatioFix = false },
+  dispatch,
+  mounted
+) => {
   // If SVG is supported
   if (typeof SVGRect !== 'undefined') {
+    const imageURL = hasSVGLocalFallback(src)
+    let prefixURL = ''
+    if (imageURL && imageURL.includes('http')) {
+      prefixURL = 'https://cors-anywhere.herokuapp.com/'
+    }
+
     // Request the SVG file to load it inline
     const ajax = new XMLHttpRequest()
-    ajax.open('GET', src, true)
+    ajax.timeout = 2500 // time in milliseconds
+    ajax.open('GET', `${prefixURL}${imageURL}`, true)
+    // ajax.overrideMimeType('image/svg+xml')
     ajax.onreadystatechange = (e: any) => {
+      if (!mounted) return
       if (ajax.readyState === 4) {
         if (ajax.status === 200) {
           dispatch({
             type: 'INLINE_SVG_LOADED',
             payload: {
-              svgInlineCode: ajax.responseText
+              svgInlineCode: ajax.responseText.replace(
+                '<svg ',
+                `<svg role="img" ${
+                  addAspectRatioFix
+                    ? 'preserveAspectRatio="xMinYMin slice"'
+                    : ''
+                }`
+              )
             }
           })
         } else {
@@ -29,6 +64,7 @@ export default (src: string, { inlineSvgFallback = null }, dispatch) => {
     }
     ajax.send()
   } else {
+    if (!mounted) return
     dispatch({
       type: 'INLINE_SVG_FAILED',
       payload: {
